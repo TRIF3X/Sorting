58dc972a9c05fd9ee87f1606b169261e584a2100
TRIF3X <thewizardofcrypto@gmail.com>
2019-02-26 15:33:26 -0600

    merge_sort done

--- project/recursive_sorting.py
+++ project/recursive_sorting.py
@@ -14,9 +14,44 @@ def merge_sort( arr ):
         merge_sort(left)
         merge_sort(right)
 
+        # Variables used when sorting
+        i = 0
+        j = 0
+        k = 0
+
+        # while the array on the left and the right is greater than the index 
+        while i < len(left) and j < len(right):
+            # if the number on the left array at the first position is less than the number on the right at the first position
+            if left[i] < right[j]:
+                # insert the number at the current position of k in the array
+                arr[k] = left[i]
+                # incrememnt i by one so that we are now looking at the next available card on the left
+                i = i+1
+            # perform the opposite operation
+            else:
+                arr[k] = right[j]
+                j = j+1
+            # increment k so that the next number inserted into the array is at the position behind the previous inserted number
+            k = k+1
+
+
+        # if everything on the right is already merged we can just merge the left to the array 
+        while i < len(left):
+            arr[k] = left[i]
+            i = i+1
+            k = k+1
+
+        # if everything on the left is already merged we can just merge the right to the array
+        while j < len(right):
+            arr[k] = right[j]
+            j = j+1
+            k = k+1
+
 
     return arr
 
+
+print(merge_sort([78, 248, 61, 233, 11, 212, 142, 91, 197, 203, 192, 111, 234, 66, 178, 38, 73, 188, 211, 114]))
 
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
